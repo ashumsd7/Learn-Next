c4264d4bfab7d5ff7332913962df1268107f4aea
aathym <aat8001@gmail.com>
2022-08-31 23:19:55 +0530

    How to add css modules

---
+++ next-meet-project/pages/[meetupId]/index.js
@@ -0,0 +1,8 @@
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return <MeetupDetail description='This is first discription' src='https://thumbs.dreamstime.com/b/meetup-neon-sign-brick-wall-background-86475691.jpg' title='MY FIRST MEET UP' address='23/34 somsunder palya '  ></MeetupDetail>;
+}
+
+export default MeetupDetails;
